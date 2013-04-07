6984b582c53cad9239cc67e3dc1b8ce8c22fb7e8
Kent Gustavsson <kent@minoris.se>
2013-04-07 15:11:22 +0200

    Spaceship
    Removed comment
    Added more precisweight
    Added spacestation output

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,12 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		SpaceShip atlantis = new SpaceShip();
 		atlantis.setName("Atlantis");
-		atlantis.setWeight(2040000.0);
+		atlantis.setWeight(2040001.0);
 		
+		System.out.println("Spacestation");
+		System.out.println("==== ==== ==== ==== ====");
 		System.out.println("Hello " + atlantis);
 
 	}
