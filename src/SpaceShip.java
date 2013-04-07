a492ced8a76fae1781b8f6d1ca14edd7c27a8c13
Kent Gustavsson <kent@minoris.se>
2013-04-07 15:10:17 +0200

    Initial Commit

---
+++ src/SpaceShip.java
@@ -0,0 +1,41 @@
+
+public class SpaceShip {
+	private String name;
+	private Double weight;
+	
+	
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "SpaceShip [name=" + name + ", weight=" + weight + "]";
+	}
+	
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the weight
+	 */
+	public Double getWeight() {
+		return weight;
+	}
+	/**
+	 * @param weight the weight to set
+	 */
+	public void setWeight(Double weight) {
+		this.weight = weight;
+	}
+	
+	
+}
