26eb0ac4ca65e422d597b2c6863953f5dfcfc82d
Kent Gustavsson <kent@minoris.se>
2013-04-07 22:11:03 +0200

    Eclipse cleanup

--- src/SpaceShip.java
+++ src/SpaceShip.java
@@ -1,41 +1,45 @@
-
 public class SpaceShip {
 	private String name;
 	private Double weight;
-	
-	
-	/* (non-Javadoc)
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see java.lang.Object#toString()
 	 */
 	@Override
 	public String toString() {
 		return "SpaceShip [name=" + name + ", weight=" + weight + "]";
 	}
-	
+
 	/**
 	 * @return the name
 	 */
 	public String getName() {
 		return name;
 	}
+
 	/**
-	 * @param name the name to set
+	 * @param name
+	 *            the name to set
 	 */
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	/**
 	 * @return the weight
 	 */
 	public Double getWeight() {
 		return weight;
 	}
+
 	/**
-	 * @param weight the weight to set
+	 * @param weight
+	 *            the weight to set
 	 */
 	public void setWeight(Double weight) {
 		this.weight = weight;
 	}
-	
-	
+
 }
